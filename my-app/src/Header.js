410c6f85c3646e5b2521df105dcd31ee8f121d53
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-08-27 18:13:03 +0300

    Fetch ile api den veri alma

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+
 function Header (){
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res)=> res.json())
+        .then((data)=> setUsers(data))
+        .finally(()=> setLoading(false))
+    },[]);
+
     return <div>
         <p>Burası header</p>
+        {loading && <p>Yükleniyor..!</p>}
+        <ul>
+            {
+                users.map((user)=>(
+                    <li key={user.id}>
+                        {user.name}
+                    </li>
+                ))  
+            }
+        </ul>
     </div>
 }
 export default Header;
